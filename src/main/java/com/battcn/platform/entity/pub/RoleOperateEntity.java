e8b362fbe053a40fe3d243e6c5349d5b492a88b8
Administrator <Administrator@LAPTOP-7346KRN3>
2016-12-14 15:44:17 +0800

    修复RoleOperateEntity实体类私有成员opid定义与数据库定义不一致，数据库定义为op_id增加时报例字段不存在

--- src/main/java/com/battcn/platform/entity/pub/RoleOperateEntity.java
+++ src/main/java/com/battcn/platform/entity/pub/RoleOperateEntity.java
@@ -1,34 +1,32 @@
+
 package com.battcn.platform.entity.pub;
 
+import javax.persistence.Column;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Table(name = "t_sys_role_operate")
-public class RoleOperateEntity implements java.io.Serializable
-{
+public class RoleOperateEntity implements java.io.Serializable {
 	@Transient
 	private static final long serialVersionUID = 7164734171717016145L;
 
 	private Integer role;
+	@Column(name = "op_id")
 	private Integer opId;
 
-	public Integer getRole()
-	{
+	public Integer getRole() {
 		return role;
 	}
 
-	public void setRole(Integer role)
-	{
+	public void setRole(Integer role) {
 		this.role = role;
 	}
 
-	public Integer getOpId()
-	{
+	public Integer getOpId() {
 		return opId;
 	}
 
-	public void setOpId(Integer opId)
-	{
+	public void setOpId(Integer opId) {
 		this.opId = opId;
 	}
 
